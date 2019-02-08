de044636eadd663ed87588424cf8ccb63415a929
shywan12 <47408315+shywan12@users.noreply.github.com>
2019-02-08 10:09:43 +0530

    Update leap1.py

--- leap1.py
+++ leap1.py
@@ -3,8 +3,8 @@ while True:
                t7=int(input())
                break
         except:
-              print('invalid')
-              break
+               print('invalid')
+               break
  if t7%400==0 or t7%4==0 and t7%100!=0:
         print('yes')
 else:
